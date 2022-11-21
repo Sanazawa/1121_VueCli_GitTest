ef4b4741b8008b9fe6c4937027d4ecb532c34fcb
https://github.com/settings/admin <tafairry520@gmail.com>
2022-11-21 16:26:50 +0800

    git init

---
+++ src/components/VeeValidate.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="v-form">
+    <VForm v-slot="{ errors, values, validate }" @submit="onSubmit">
+      {{ errors }} {{ values }}
+
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+
+        <VField
+          id="email"
+          name="email"
+          type="email"
+          class="form-control"
+          :class="{ 'is-invalid': errors['email'] }"
+          placeholder="請輸入 Email"
+          rules="email|required"
+          v-model="user.email"
+        ></VField>
+
+        <error-message name="email" class="invalid-feedback"></error-message>
+      </div>
+
+      <button
+        class="btn me-2 btn-outline-primary"
+        type="button"
+        @click="validate"
+      >
+        驗證
+      </button>
+
+      <button class="btn btn-primary" type="submit">Submit</button>
+    </VForm>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VForm',
+  data() {
+    return {
+      user: {},
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.user);
+    },
+  },
+  created() {
+    console.log(this);
+  },
+};
+</script>
